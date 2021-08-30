768c34a7c6b1f8d48e859fd28c980335eb540c92
Valentine <humer23@gmail.com>
2021-08-30 12:53:43 +0300

    edit mobile home page, create hamburger menu

---
+++ src/js/menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.burger_menu').click(function() {
+        $('.burger_menu').toggleClass('open-menu');
+        $('.nav_menu').toggleClass('open-menu');
+    });
+});
